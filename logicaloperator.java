88c3ffaf799cab6ede8cc534e32b975b5a07e9f8
sv742744 <shashi09op@gmail.com>
2024-12-03 15:58:41 +0530

    Add files via upload

---
+++ logicaloperator.java
@@ -0,0 +1,11 @@
+class logical {
+    public static void main(String [] args){
+        System.out.println("logical AND ::");
+        System.out.println(true&false);
+        System.out.println("logical OR ::");
+        System.out.println(true|false);
+        System.out.println("logical NOT");
+        System.out.println(!(false));
+
+    }
+}
