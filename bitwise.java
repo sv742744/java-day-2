13bfdf4265d143ea7b753afc33cc9e8a6005a463
sv742744 <shashi09op@gmail.com>
2024-12-03 14:22:33 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class bitwise {
+    public static void main (String [] args ){
+        System.out.println("bit wise AND &  :");
+        System.out.println(10&12);
+        System.out.println(2&18);
+        System.out.println(18&15);
+        System.out.println(16&14);
+        System.out.println(12&11);
+        System.out.println(13&18);
+        System.out.println("bit wise OR  |  :");
+        System.out.println(11|18);
+        System.out.println(22|144);
+        System.out.println(88|122);
+        System.out.println("bit wise XOR ^ :");
+        System.out.println(122^122);
+        System.out.println(130^140);
+        System.out.println(120^130);
+        System.out.println(110^110);
+        System.out.println(5^5);
+
+
+    }
+} 
