3d7d918afd87639be64425ebd18b4d385e4c26f5
sv742744 <shashi09op@gmail.com>
2024-12-03 10:29:22 +0530

    Add files via upload

---
+++ comparisionoperators.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class comparisionoperators  {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a value =");
+        int a=sc.nextInt();
+        System.out.println("enter b value =");
+        int b=sc.nextInt();
+        System.out.println("a is equal to b            : "+(a==b));
+        System.out.println("a is not equal to b        : "+(a!=b));
+        System.out.println("a is less or eual to b     : "+(a<=b));
+        System.out.println("a is gratter or equal to b : "+(a>=b));
+        System.out.println("a is lees than b           : "+(a<b));
+        System.out.println("a is greter than b         : "+(a>b));
+        
+
+
+
+    }
+}
