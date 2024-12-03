dabe8a6c25a7c76bbc808defd12f6ddcc14ce592
sv742744 <shashi09op@gmail.com>
2024-12-03 15:58:26 +0530

    Add files via upload

---
+++ flipbit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class flipbit {
+    public static void main (String [] args ){
+        Scanner s=new Scanner(System.in);
+        /*int a=s.nextInt();
+        int p=s.nextInt();
+        System.out.println(a^(1<<p));*/
+        System.out.println(15<<2);
+    }
+}
